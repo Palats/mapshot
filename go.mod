5dd9b3afaf76d82c378dfa5a98dbb85f586c98c1
Pierre Palatin <pierre@palatin.fr>
2020-09-08 20:40:50 +0200

    Discovery of data dir / binary.

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/Palats/mapshot
 
 go 1.13
 
-require github.com/spf13/cobra v1.0.0
+require (
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/prometheus/common v0.4.0
+	github.com/spf13/cobra v1.0.0
+	github.com/spf13/pflag v1.0.3
+)
--- mapshot.go
+++ mapshot.go
@@ -2,17 +2,90 @@ package main
 
 import (
 	"archive/zip"
+	goflag "flag"
 	"fmt"
 	"os"
 	"path"
 
 	"github.com/Palats/mapshot/embed"
+	"github.com/golang/glog"
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	flag "github.com/spf13/pflag"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "mapshot",
 	Short: "mapshot generates zoomable screenshot for Factorio",
+}
+
+var factorioDataDir = rootCmd.PersistentFlags().String("datadir", "", "Path to factorio data dir. Try default locations if empty.")
+var factorioBinary = rootCmd.PersistentFlags().String("binary", "", "Path to factorio binary. Try default locations if empty.")
+
+func findDataDir() string {
+	if *factorioDataDir != "" {
+		return *factorioDataDir
+	}
+
+	candidates := []string{
+		`~/.factorio`,
+		`~/factorio`,
+		`~/Library/Application Support/factorio`,
+		`%appdata%\Factorio`,
+	}
+	for _, c := range candidates {
+		s, err := homedir.Expand(c)
+		if err != nil {
+			glog.Infof("Unable to expand %s: %v", c, err)
+			continue
+		}
+		info, err := os.Stat(s)
+		if os.IsNotExist(err) {
+			glog.Infof("Path %s does not exists, skipped", s)
+			continue
+		}
+		if !info.IsDir() {
+			glog.Infof("Path %s is a file, skipped", s)
+		}
+		glog.Infof("Found factorio data dir: %s", s)
+		return s
+	}
+	glog.Infof("No Factorio data dir found")
+	return ""
+}
+
+func findBinary() string {
+	if *factorioBinary != "" {
+		return *factorioBinary
+	}
+	candidates := []string{
+		`~/.factorio/bin/x64/factorio`,
+		`~/factorio/bin/x64/factorio`,
+		`~/Library/Application Support/Steam/steamapps/common/Factorio/factorio.app/Contents`,
+		`/Applications/factorio.app/Contents`,
+		`C:\Program Files (x86)\Steam\steamapps\common\Factorio`,
+		`C:\Program Files\Factorio`,
+	}
+	for _, c := range candidates {
+		s, err := homedir.Expand(c)
+		if err != nil {
+			glog.Infof("Unable to expand %s: %v", c, err)
+			continue
+		}
+		info, err := os.Stat(s)
+		if os.IsNotExist(err) {
+			glog.Infof("Path %s does not exists, skipped", s)
+			continue
+		}
+		if info.IsDir() {
+			glog.Infof("Path %s is a directory, skipped", s)
+			continue
+		}
+		glog.Infof("Factorio binary found: %s", s)
+		return s
+	}
+	glog.Infof("No factorio binary found")
+	return ""
 }
 
 var cmdVersion = &cobra.Command{
@@ -41,10 +114,10 @@ var cmdPackage = &cobra.Command{
 		for filename, content := range embed.ModFiles {
 			f, err := w.Create(path.Join(name, filename))
 			if err != nil {
-				return fmt.Errorf("unable to add %q to zip file: %w", err)
+				return fmt.Errorf("unable to add %q to zip file: %w", filename, err)
 			}
 			if _, err = f.Write([]byte(content)); err != nil {
-				return fmt.Errorf("unable to write %q to zip file: %w", err)
+				return fmt.Errorf("unable to write %q to zip file: %w", filename, err)
 			}
 		}
 		if err := w.Close(); err != nil {
@@ -54,9 +127,24 @@ var cmdPackage = &cobra.Command{
 	},
 }
 
+var cmdInfo = &cobra.Command{
+	Use:   "info",
+	Short: "Show info about what mapshot knows.",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println("datadir:", findDataDir())
+		fmt.Println("binary:", findBinary())
+		return nil
+	},
+}
+
 func main() {
+	flag.CommandLine.AddGoFlagSet(goflag.CommandLine)
+	flag.Parse()
+
 	rootCmd.AddCommand(cmdPackage)
 	rootCmd.AddCommand(cmdVersion)
+	rootCmd.AddCommand(cmdInfo)
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
