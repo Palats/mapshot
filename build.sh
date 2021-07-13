effe8ce0f72ffb3ce44ae0ffe034561a47db55b1
Pierre Palatin <pierre@palatin.fr>
2021-07-13 20:52:34 +0200

    Add ARM builds (linux+darwin).
    The binaries are untested.
    
    Fixes #13

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ go test ./... || exit 1
 TARGET="$(pwd)/build"
 mkdir -p "${TARGET?}"
 GOOS=linux GOARCH=amd64 go build -o "${TARGET?}/mapshot-linux" mapshot.go
+GOOS=linux GOARCH=arm go build -o "${TARGET?}/mapshot-linux-arm" mapshot.go
 GOOS=windows GOARCH=amd64 go build -o "${TARGET?}/mapshot-windows.exe" mapshot.go
 GOOS=darwin GOARCH=amd64 go build -o "${TARGET?}/mapshot-darwin" mapshot.go
+GOOS=darwin GOARCH=arm64 go build -o "${TARGET?}/mapshot-darwin-arm64" mapshot.go
 go run mapshot.go package "${TARGET?}"
