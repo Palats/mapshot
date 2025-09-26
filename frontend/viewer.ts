49b783ecddb102fd64a282bd353a928530f07bdf
ungaul <kxvnlpl+github@proton.me>
2025-09-26 20:38:20 +0200

    css+js
    I initially edited the JS of a rendered map to make the JS in this file, so it may have errors. I will provide separately an example of the rendered mapshot webui with the changes I made.

--- frontend/viewer.ts
+++ frontend/viewer.ts
@@ -26,6 +26,259 @@ common.globalCSS(`
         width:30px;
         height:30px;
     }
+
+    :root {
+    --first: #000;
+    --second: #fff;
+    --third: #292929;
+    --link-cursor: pointer;
+    }
+
+    * {
+        color: var(--second);
+        font-family: DejaVu Sans, sans-serif;
+        user-select: none;
+    }
+
+    body {
+        background: var(--first);
+    }
+
+    #content {
+        height: 100%;
+    }
+
+    .leaflet-control-boxzoom,
+    .leaflet-control-zoomslider {
+        display: none;
+    }
+
+    #menu {
+        position: fixed;
+        top: 0;
+        left: 0;
+        background: #252425;
+        z-index: 1000;
+    }
+
+    #menu div {
+        background: #161516;
+    }
+
+    .leaflet-control.leaflet-control-layers {
+        background: #252425;
+        border-radius: 0;
+        border: 1px solid var(--third);
+        padding: 0;
+    }
+
+    input[type="radio"] {
+        display: none;
+    }
+
+    span {
+        text-transform: capitalize;
+    }
+
+    .leaflet-control-layers-separator {
+        display: none;
+    }
+
+    .leaflet-control-layers-list::before {
+        content: "Surfaces";
+        font-size: 15px;
+    }
+
+    #planets::before {
+        content: "Planets";
+    }
+
+    #platforms::before {
+        content: "Platforms";
+    }
+
+    label,
+    .leaflet-control-layers-list::before,
+    #planets::before,
+    #platforms::before {
+        border-bottom: 1px solid #161516;
+        padding: 2px 7px;
+        display: flex !important;
+        align-items: center;
+    }
+
+    label div,
+    label span {
+        display: flex;
+        width: 100%;
+        gap: 5px;
+    }
+
+    .leaflet-control-layers-overlays label,
+    .leaflet-control-layers-overlays div {
+        width: 30px;
+        height: 30px;
+        align-items: center;
+        justify-content: center;
+        padding: 0;
+        margin: 0;
+    }
+
+    .leaflet-control-layers-overlays img,
+    .leaflet-control-layers-overlays span {
+        align-items: center;
+        justify-content: center;
+        display: flex;
+    }
+
+    label:hover {
+        background: #ffc75a;
+        cursor: var(--link-cursor);
+    }
+
+    label:hover span {
+        color: var(--first);
+    }
+
+    .leaflet-right {
+        left: 0 !important;
+        right: unset !important;
+    }
+
+    .leaflet-control-layers-overlays {
+        position: fixed;
+        right: 0;
+        top: 0;
+        background: #252425;
+        box-shadow: 0 0 10px var(--first);
+        /* border: 2px solid var(--first); */
+    }
+
+    /* .leaflet-control-layers-base label:nth-child(6)::before {
+        content: "Platforms";
+    } */
+
+    .leaflet-control-layers-list::before,
+    #planets::before,
+    #platforms::before {
+        color: #f8ecc1;
+    }
+
+    .leaflet-touch .leaflet-control-layers,
+    .leaflet-touch .leaflet-bar {
+        border: none;
+    }
+
+    .leaflet-control-layers-list {
+        box-shadow: 0 0 10px var(--first);
+        /* border: 2px solid var(--first); */
+        outline: none;
+        width: 200px;
+    }
+
+    .leaflet-left .leaflet-control,
+    .leaflet-top .leaflet-control {
+        margin: 0 !important;
+    }
+
+    label.active {
+        background: #ffc75a;
+    }
+
+    label.active span {
+        color: var(--first);
+    }
+
+    .icon {
+        width: 14px;
+        height: 14px;
+    }
+
+    .leaflet-tooltip {
+        display: flex;
+        align-items: center;
+        gap: 5px;
+        background: none;
+        border: none;
+        color: var(--second);
+        box-shadow: none;
+        text-shadow:
+            -2px -2px 0 black,
+            2px -2px 0 black,
+            -2px 2px 0 black,
+            2px 2px 0 black;
+    }
+
+    .leaflet-tooltip:before {
+        display: none;
+    }
+
+    .layer-toggle {
+        display: flex;
+        align-items: center;
+        background: #333;
+        color: white;
+        border: none;
+        padding: 5px 10px;
+        margin: 5px;
+        cursor: pointer;
+    }
+
+    .layer-toggle.active {
+        background: #ffc75a;
+    }
+
+    .layer-icon {
+        width: 20px;
+        object-fit: contain;
+        height: 20px;
+    }
+
+    input[type="checkbox"] {
+        display: none;
+    }
+
+    .leaflet-bottom.leaflet-right {
+        display: none;
+    }
+
+    .active span div {
+        background: #ffc75a;
+    }
+
+    label:has(span.active) {
+        background: #ffc75a;
+    }
+
+    label:has(input[type="checkbox"]:checked) {
+        background: #ffc75a;
+    }
+
+    span.active {
+        color: var(--first);
+    }
+
+    #download {
+        position: fixed;
+        bottom: 20px;
+        font-size: 40px;
+        right: 20px;
+        z-index: 1000;
+        border-radius: 100px;
+        cursor: var(--link-cursor);
+        background: var(--third);
+        transition: 0.3s;
+    }
+
+    #download:hover {
+        filter: brightness(0.9);
+    }
+
+    @media screen and (max-width: 768px) {
+        .leaflet-control-layers-list{
+            width: 120px;
+        }
+    }
 `);
 
 /*
@@ -74,10 +327,16 @@ class Surface {
         let stationsLayers = [];
         if (common.isIterable(si.stations)) {
             for (const station of si.stations as Iterable<common.FactorioStation>) {
-                stationsLayers.push(L.marker(
-                    this.midPointToLatLng(station.bounding_box),
-                    { title: station.backer_name },
-                ).bindTooltip(station.backer_name, { permanent: true }))
+                let tooltipContent = replaceItemAndFluidTags(station.backer_name);
+                stationsLayers.push(
+                    L.marker(
+                        this.midPointToLatLng(station.bounding_box),
+                        {
+                            title: station.backer_name,
+                            icon: createCustomIcon("https://raw.githubusercontent.com/ungaul/factorio-resources/main/bonus/64x64/station.png")
+                        }
+                    ).bindTooltip(tooltipContent, { permanent: true })
+                );
             }
         }
         this.trainLayer = L.layerGroup(stationsLayers);
@@ -86,18 +345,25 @@ class Surface {
         let tagsLayers = [];
         if (common.isIterable(si.tags)) {
             for (const tag of si.tags as Iterable<common.FactorioTag>) {
-                tagsLayers.push(L.marker(
-                    this.worldToLatLng(tag.position.x, tag.position.y),
-                    { title: `${tag.force_name}: ${tag.text}` },
-                ).bindTooltip(tag.text, { permanent: true }))
+                let tooltipContent = replaceItemAndFluidTags(tag.text);
+                tagsLayers.push(
+                    L.marker(
+                        this.worldToLatLng(tag.position.x, tag.position.y),
+                        {
+                            title: `${tag.force_name}: ${tag.text}`,
+                            icon: createCustomIcon("https://raw.githubusercontent.com/ungaul/factorio-resources/main/bonus/64x64/tag.png")
+                        }
+                    ).bindTooltip(tooltipContent, { permanent: true })
+                );
             }
         }
         this.tagsLayer = L.layerGroup(tagsLayers);
 
         // Layer: debug
         const debugLayers = [
-            L.marker([0, 0], { title: "Start" }).bindPopup("Starting point"),
-        ]
+            L.marker([0, 0], { title: "Start", icon: createCustomIcon("https://raw.githubusercontent.com/ungaul/factorio-resources/main/bonus/64x64/debug.png") }).bindPopup("Starting point"),
+        ];
+
         if (common.isIterable(si.players)) {
             for (const player of si.players as Iterable<common.FactorioPlayer>) {
                 debugLayers.push(
@@ -105,16 +371,17 @@ class Surface {
                         this.worldToLatLng(player.position.x, player.position.y),
                         {
                             title: player.name,
-                            alt: `Player: ${player.name}`
-                        },
-                    ).bindTooltip(player.name, {
-                        permanent: true
-                    })
-                )
+                            alt: `Player: ${player.name}`,
+                            icon: createCustomIcon("https://raw.githubusercontent.com/ungaul/factorio-resources/main/bonus/64x64/debug.png")
+                        }
+                    ).bindTooltip(player.name, { permanent: true })
+                );
             }
         }
         if (si.player) {
-            debugLayers.push(L.marker(this.worldToLatLng(si.player.x, si.player.y), { title: "Player" }).bindPopup("Player"));
+            debugLayers.push(
+                L.marker(this.worldToLatLng(si.player.x, si.player.y), { title: "Player", icon: createCustomIcon("https://raw.githubusercontent.com/ungaul/factorio-resources/main/bonus/64x64/debug.png") }).bindPopup("Player")
+            );
         }
         debugLayers.push(
             L.marker(this.worldToLatLng(si.world_min.x, si.world_min.y), { title: `${si.world_min.x}, ${si.world_min.y}` }),
@@ -150,25 +417,113 @@ class Surface {
 
 }
 
+function createCustomIcon(url: string, size = [16, 16]): L.Icon {
+    return L.icon({
+        iconUrl: url,
+        iconSize: size,
+        iconAnchor: [0, 5],
+        popupAnchor: [0, 0]
+    });
+}
+
+function replaceItemAndFluidTags(text: string): string {
+    return text.replace(/\[(item|fluid|recipe)=(.*?)\]/g,
+        (match, type, name) =>
+            `<img class="icon" src="https://raw.githubusercontent.com/ungaul/factorio-resources/main/icons/64x64/${name}.png" alt="${name}" title="${name}">`
+    );
+}
+
 function run(config: common.MapshotConfig, info: common.MapshotJSON) {
     const layerControl = L.control.layers();
 
+    interface SurfaceCategory {
+        layer: L.TileLayer;
+        name: string;
+        className: string;
+        category: string;
+    }
     const surfaces: Surface[] = [];
     const surfaceByKey: Map<string, Surface> = new Map();
+    // Collect categorized surfaces for planet/platform UI
+    const categories: { planets: SurfaceCategory[]; platforms: SurfaceCategory[]; layers: SurfaceCategory[] } = {
+        planets: [],
+        platforms: [],
+        layers: []
+    };
+
+    // populate categories and surfaces
     for (const si of info.surfaces) {
         const s = new Surface(config, si);
         surfaces.push(s);
-        layerControl.addBaseLayer(s.baseLayer, si.surface_name);
+
+        // Categorize by surface name
+        let cat: "planets" | "platforms" | "layers" = "planets";
+        let className = "planet";
+        let category = "#planets";
+        if (si.surface_name.startsWith("platform-")) {
+            cat = "platforms";
+            className = "platform";
+            category = "#platforms";
+        }
+
+        categories[cat].push({
+            layer: s.baseLayer,
+            name: si.surface_name,
+            className,
+            category
+        });
+
         surfaceByKey.set(s.surfaceInfo.surface_idx.toString(), s);
         surfaceByKey.set(s.surfaceInfo.surface_name, s);
     }
 
+    // Sorting helper
+    function customSort(array: SurfaceCategory[]) {
+        return array.sort((a, b) => {
+            const [nameA, nameB] = [a.name.toLowerCase(), b.name.toLowerCase()];
+            const [matchA, matchB] = [nameA.match(/^platform-(\d+)$/), nameB.match(/^platform-(\d+)$/)];
+
+            if (!matchA && matchB) return -1;
+            if (matchA && !matchB) return 1;
+            if (nameA === "vulcanus") return 1;
+            if (nameB === "vulcanus") return -1;
+            return matchA && matchB ? Number(matchA[1]) - Number(matchB[1]) : nameA.localeCompare(nameB);
+        });
+    }
+
+    function addCategory(categoryId: keyof typeof categories, maps: SurfaceCategory[]) {
+        customSort(maps).forEach(item => {
+            let icon = categoryId === "planets"
+                ? item.name
+                : "cargo-pod";
+            let displayName =
+                `<span class="${item.className}">
+                <img src="https://raw.githubusercontent.com/ungaul/factorio-resources/main/icons/64x64/${icon}.png"
+                     width="16" height="16" style="vertical-align: middle; margin-right: 5px;"
+                     onerror="this.onerror=null;this.src='https://raw.githubusercontent.com/ungaul/factorio-resources/main/icons/64x64/cargo-pod.png';">
+                ${item.name}
+            </span>`;
+            layerControl.addBaseLayer(item.layer, displayName);
+        });
+    }
+    addCategory("planets", categories.planets);
+    addCategory("platforms", categories.platforms);
+
     const trainLayer = L.layerGroup();
     const tagsLayer = L.layerGroup();
     const debugLayer = L.layerGroup();
-    layerControl.addOverlay(trainLayer, "Train stations");
-    layerControl.addOverlay(tagsLayer, "Tags");
-    layerControl.addOverlay(debugLayer, "Debug");
+    layerControl.addOverlay(
+        trainLayer,
+        "<img class='layer-icon' src='https://raw.githubusercontent.com/ungaul/factorio-resources/main/bonus/64x64/station.png'>"
+    );
+    layerControl.addOverlay(
+        tagsLayer,
+        "<img class='layer-icon' src='https://raw.githubusercontent.com/ungaul/factorio-resources/main/bonus/64x64/tag.png'>"
+    );
+    layerControl.addOverlay(
+        debugLayer,
+        "<img class='layer-icon' src='https://raw.githubusercontent.com/ungaul/factorio-resources/main/bonus/64x64/debug.png'>"
+    );
     const overlayKeys = new Map<L.Layer, string>();
     overlayKeys.set(trainLayer, "lt");
     overlayKeys.set(tagsLayer, "lg");
@@ -192,6 +547,15 @@ function run(config: common.MapshotConfig, info: common.MapshotJSON) {
         zoomDelta: 1.0,
     });
     layerControl.addTo(mymap);
+
+    setTimeout(() => {
+        document.querySelectorAll(".planet, .platform").forEach(item => {
+            item.addEventListener("click", (ev) => {
+                document.querySelectorAll(".planet, .platform").forEach(el => el.classList.remove("active"));
+                (ev.currentTarget as HTMLElement).classList.add("active");
+            });
+        });
+    }, 500);
 
     // Add a control to zoom to a region.
     L.Control.boxzoom({
@@ -231,6 +595,12 @@ function run(config: common.MapshotConfig, info: common.MapshotJSON) {
         queryParams.set("s", currentSurface.surfaceInfo.surface_idx.toString());
         history.replaceState(null, "", "?" + queryParams.toString());
     });
+
+    let minBounds = currentSurface.worldToLatLng(currentSurface.surfaceInfo.world_min.x, currentSurface.surfaceInfo.world_min.y);
+    let maxBounds = currentSurface.worldToLatLng(currentSurface.surfaceInfo.world_max.x, currentSurface.surfaceInfo.world_max.y);
+    let bounds = L.latLngBounds(minBounds, maxBounds);
+    mymap.setMaxBounds(bounds);
+    (mymap as any).options.maxBoundsViscosity = 1.0;
 
     // Update URL when position/view changes.
     const onViewChange = (e: L.LeafletEvent) => {
